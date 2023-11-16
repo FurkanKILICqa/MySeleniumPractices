23dbc186b0e8765da26fdded0d600be0dd83f371
Furkan Kılıc <fkrx928@gmail.com>
2023-11-16 18:09:45 +0300

    my commit

--- src/test/java/MyPractices/C03.java
+++ src/test/java/MyPractices/C03.java
@@ -9,28 +9,22 @@ import utilities.TestBase;
 
 public class C03 extends TestBase {
 
-    //===========Homework 2==========================
-
-    //- Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
-    //- Çizimden sonra clear butonuna basınız
-    //- Sayfayi kapatiniz
-
-
     @Test
     public void test01() {
 
         //- http://szimek.github.io/signature_pad/ sayfasına gidiniz
         driver.get("http://szimek.github.io/signature_pad/");
 
-       WebElement drowing = driver.findElement(By.xpath("//*[@style='touch-action: none; user-select: none;']"));
 
+        //- Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
+       WebElement drowing = driver.findElement(By.xpath("//*[@style='touch-action: none; user-select: none;']"));
         Actions actions = new Actions(driver);
         actions.clickAndHold(drowing).moveByOffset(48,120).release(drowing).perform();
 
 
 
-
-
+        //- Çizimden sonra clear butonuna basınız
+        driver.findElement(By.xpath("//*[@class='button clear']")).click();
 
 
 
